cf381bd0c4b488e5c17e02d272acb46a9abf41a4
Alex Chen <112623chen@gmail.com>
2022-12-10 20:49:56 -0500

    chore():

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
 import streamlit as st 
+import pandas as pd 
+import numpy as np
+import pickle
+from src.helpers.functions import *
+
+data = pd.read_csv("src/data/listings_Sep_22.csv", low_memory=False)
+model = pickle.load(open('./src/model/reducedLinearRegressionModel.pkl','rb'))
+not_important_features = open("./src/helpers/notImportantFeatures.txt", "r").read().split("\n")[:-1]
+important_columns = ['host_location','host_response_time','host_response_rate','neighbourhood_cleansed','neighbourhood_group_cleansed','latitude','longitude','property_type','room_type','accommodates','bedrooms','beds','instant_bookable',]
+df = create_df(data, important_columns, True)
+
+prediction = None
+
+st.set_page_config(
+    page_title="Airbnb Listing Price Predictor",
+    layout="wide"
+)
+
+c1, c2, c3 = st.columns(3, gap='medium')
+
+with c1:
+    st.header("Listing Features Info")
+    propertyType = st.selectbox("Property Type",data.property_type.value_counts().index)
+    roomType = st.selectbox("Room Type", data.room_type.value_counts().index)
+    maxCapacity = st.number_input("Max Capacity of Listing", int(df['accommodates'].min()), int(df['accommodates'].max()), 1, 1)
+    numberOfBedrooms = st.number_input("Number of Bedroom(s)",int(df['bedrooms'].min()), int(df['bedrooms'].max()), step=1)
+    numberOfBeds = st.number_input("Number of Bed(s)", int(df['beds'].min()), int(df['beds'].max()), step=1)
+    isInstantBookable = st.checkbox("Will be Instant Bookable")
+
+with c2:
+    st.header("Listing Location Info")
+    borough = st.selectbox("Borough", data.neighbourhood_group_cleansed.value_counts().index)
+    neighborhood = st.selectbox("Neighborhood", data[data.neighbourhood_group_cleansed == borough].neighbourhood_cleansed.value_counts().index)
+    lat = (data.groupby('neighbourhood_cleansed')['latitude'].get_group(neighborhood).mean())
+    log = (data.groupby('neighbourhood_cleansed')['longitude'].get_group(neighborhood).mean())
+    st.header("Host Info")
+    hostLocation = st.text_input("Host Location")
+    hostResponseTime = st.selectbox("Host Response Time", data.host_response_time.value_counts().index)
+    hostResponseRate = st.number_input('Host Response Rate Percentage', 0, 100, 90, 1)
+
+with c3:
+    st.header("Prediction")
+    if st.button("Make Prediction"):
+        input_array = np.array([hostLocation, hostResponseTime, hostResponseRate, borough, neighborhood, lat, log, propertyType, roomType, maxCapacity, numberOfBedrooms, numberOfBeds, isInstantBookable], dtype=object)
+        prediction = get_prediction(model, data, important_columns, not_important_features, input_array)[0]
+        st.header(f"The predicted price for this listing for one day is ${round(prediction, 2)}")
---
+++ src/helpers/functions.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+#Create helper DataFrame
+def create_df(data, important_columns, flag):
+    if flag:
+        data['host_response_rate'] = pd.to_numeric(data['host_response_rate'].str.replace("%",''))
+        data['instant_bookable'] = np.where(data['instant_bookable'] == 't', True, False)
+    df = data[important_columns]
+    df = df.dropna().drop_duplicates().reset_index(drop=True)
+    return df
+
+#Create Feature Engineering Function:
+def get_dummies(df):
+    df = pd.get_dummies(df, columns=['instant_bookable'], drop_first=True)
+    df = pd.get_dummies(df, columns=['room_type','property_type'], drop_first=True)
+    df = pd.get_dummies(df, columns=['neighbourhood_cleansed','neighbourhood_group_cleansed'], drop_first=True)
+    df = pd.get_dummies(df, columns=['host_response_time'], drop_first=True)
+    df['host_location_in_ny'] = np.where(df['host_location'].str.contains("NY|New York"), True, False)
+    df.drop("host_location", axis=1, inplace=True)
+    df = pd.get_dummies(df, columns=['host_location_in_ny'], drop_first=True)
+    return df
+
+#Convert User Input Into Array
+def convert_input(data, important_columns, input_array, not_important_features):
+    df = create_df(data, important_columns, False)
+    df2 = pd.DataFrame(np.vstack((df.values, input_array)), columns=df.columns)
+    df2 = get_dummies(df2)
+    not_important_features.append('neighbourhood_cleansed_Manhattan')
+    not_important_features.append('neighbourhood_group_cleansed_Harlem')
+    df2 = df2.drop(not_important_features, axis=1)
+    input_data = df2.iloc[-1].values
+    return input_data
+
+#Making prediction: get_prediction
+def get_prediction(model, data, important_columns, not_important_features, input_array):
+    input_data = convert_input(data, important_columns, input_array, not_important_features)
+    prediction = model.predict([input_data])
+    return prediction
+
